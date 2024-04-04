c338d5b9b4504abd1cc5dd4dd1d5b113624e65d0
Mayra Pena <mayraapena2016@gmail.com>
2024-04-03 18:28:54 -0700

    Add profiles.yml

--- orchestrate/dags/sample_dag.py
+++ orchestrate/dags/sample_dag.py
@@ -5,20 +5,20 @@ from operators.datacoves.dbt import DatacovesDbtOperator
 
 @dag(
     default_args={
-        "start_date": datetime.datetime(2023, 1, 1, 0, 0),
-        "owner": "Mayra Pena",
-        "email": "mayraapena2016@gmail.com",
-        "email_on_failure": True,
+        'start_date': datetime.datetime(2023, 1, 1, 0, 0),
+        'owner': 'Mayra Pena',
+        'email': 'mayraapena2016@gmail.com',
+        'email_on_failure': True,
     },
-    description="Sample DAG for dbt build",
-    schedule_interval="0 0 1 */12 *",
-    tags=["version_2"],
+    description='A DAG to run dbt for the Jaffle Shop project',
+    schedule_interval='0 7 * * *',
+    tags=['version_3'],
     catchup=False,
 )
 def yaml_dbt_dag():
     run_dbt = DatacovesDbtOperator(
-        task_id="run_dbt",
-        bash_command="dbt run"
+        task_id='run_dbt',
+        bash_command='dbt run'
     )
 
 dag = yaml_dbt_dag()

54d453081b2ed4fabf0424e1c75d744606500491
Mayra Pena <mayraapena2016@gmail.com>
2024-04-04 17:44:14 -0700

    Testing multiple emails

--- orchestrate/dags/sample_dag.py
+++ orchestrate/dags/sample_dag.py
@@ -7,7 +7,7 @@ from operators.datacoves.dbt import DatacovesDbtOperator
     default_args={
         'start_date': datetime.datetime(2023, 1, 1, 0, 0),
         'owner': 'Mayra Pena',
-        'email': 'mayraapena2016@gmail.com',
+        'email': ['mayraapena2016@gmail.com', 'mayra@datacoves.com'],
         'email_on_failure': True,
     },
     description='A DAG to run dbt for the Jaffle Shop project',
@@ -18,7 +18,7 @@ from operators.datacoves.dbt import DatacovesDbtOperator
 def yaml_dbt_dag():
     run_dbt = DatacovesDbtOperator(
         task_id='run_dbt',
-        bash_command='dbt source freshness'
+        bash_command='dbt build'
     )
 
 dag = yaml_dbt_dag()

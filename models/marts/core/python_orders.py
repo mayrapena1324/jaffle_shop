5fa7fdbf4e0850bc88a70ae63fc04352cb18c2fa
Mayra Pena <mayraa.penaa@gmail.com>
2024-04-22 16:00:17 -0700

    Add python model

--- models/marts/core/python_orders.py
+++ models/marts/core/python_orders.py
@@ -1,16 +1,38 @@
-import pandas as pd
+import snowflake.snowpark.functions as F
+
 
 def model(dbt, session):
-    # Load data from the source orders table
-    source_orders_df = dbt.ref("stg_orders")
 
-    # Convert 'order_date' from string to datetime
-    source_orders_df['order_date'] = pd.to_datetime(source_orders_df['order_date'], format='%Y-%m-%d %H:%M:%S')
 
-    # Add a new column 'days_since_order' calculating the number of days from 'order_date' to today
-    source_orders_df['days_since_order'] = (pd.Timestamp('now') - source_orders_df['order_date']).dt.days
+   stg_orders_df = dbt.ref('stg_orders')
+   stg_payments_df = dbt.ref('stg_payments')
 
-    # Clean 'status' by capitalizing and stripping white spaces
-    source_orders_df['status'] = source_orders_df['status'].str.upper().str.strip()
 
-    return source_orders_df
+   payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card']
+
+
+   agg_list = [F.sum(F.when(stg_payments_df.payment_method == payment_method, stg_payments_df.amount).otherwise(0)).alias(payment_method + '_amount') for payment_method in payment_methods]
+
+
+   agg_list.append(F.sum(F.col('amount')).alias('total_amount'))
+
+
+   order_payments_df = (
+       stg_payments_df
+       .group_by('order_id')
+       .agg(*agg_list)
+   )
+
+   final_df = (
+       stg_orders_df
+       .join(order_payments_df, stg_orders_df.order_id == order_payments_df.order_id, 'left')
+       .select(stg_orders_df.order_id.alias('order_id'),
+               stg_orders_df.customer_id.alias('customer_id'),
+               stg_orders_df.order_date.alias('order_date'),
+               stg_orders_df.status.alias('status'),
+               *[F.col(payment_method + '_amount') for payment_method in payment_methods],
+               order_payments_df.total_amount.alias('amount')
+       )
+   )
+
+   return final_df
